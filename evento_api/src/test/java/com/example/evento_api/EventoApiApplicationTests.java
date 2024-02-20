63bc21fc5b76c4aec51d2e0f2b9dc770e44afe49
Pablo <jdomma863@g.educaand.es>
2024-02-20 17:18:37 +0100

    Spring Boot vacio

---
+++ evento_api/src/test/java/com/example/evento_api/EventoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
